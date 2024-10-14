3fd011fd51f49e373a0a5f7bd7ca3d1dc953dd72
Roc.Lai <laipeng668@qq.com>
2024-10-14 12:50:37 +0800

    Update roc-script.sh

--- roc-script.sh
+++ roc-script.sh
@@ -1,8 +1,10 @@
 # 移除要替换的包
 rm -rf feeds/packages/net/alist
+rm -rf feeds/luci/applications/luci-app-alist
 
 # Alist
-git clone https://github.com/sbwml/luci-app-alist packages/net/alist
+git clone https://github.com/sbwml/luci-app-alist/alist packages/net/alist
+git clone https://github.com/sbwml/luci-app-alist/luci-app-alist /luci/applications/luci-app-alist
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
