0adb15857640598b850e041a9c4e9d0e1d2a4d3e
Roc.Lai <laipeng668@qq.com>
2024-10-14 12:37:33 +0800

    Update roc-script.sh

--- roc-script.sh
+++ roc-script.sh
@@ -1,10 +1,8 @@
 # 移除要替换的包
-rm -rf feeds/packages/lang/golang
 rm -rf feeds/packages/net/alist
 
 # Alist
-git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
-git clone https://github.com/sbwml/luci-app-alist package/alist
+git clone https://github.com/sbwml/luci-app-alist packages/net/alist
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
