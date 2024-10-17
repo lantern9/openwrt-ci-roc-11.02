b220346d1e19d126155cfddb14856d8ebd5c5320
Roc.Lai <laipeng668@qq.com>
2024-10-17 15:57:40 +0800

    Update alist-script.sh

--- alist-script.sh
+++ alist-script.sh
@@ -3,7 +3,7 @@ rm -rf feeds/packages/net/alist
 rm -rf feeds/luci/applications/luci-app-alist
 
 # Alist
-git clone --depth=1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
+git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
