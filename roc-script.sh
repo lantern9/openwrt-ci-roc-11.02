531fece393c377bef24c07f1c7c8d29eb82a6651
Roc.Lai <laipeng668@qq.com>
2024-10-19 06:34:14 +0800

    Delete roc-script.sh

--- roc-script.sh
+++
@@ -1,9 +0,0 @@
-# 移除要替换的包
-rm -rf feeds/packages/net/alist
-rm -rf feeds/luci/applications/luci-app-alist
-
-# Alist
-git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
-
-./scripts/feeds update -a
-./scripts/feeds install -a
