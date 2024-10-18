55838b5bd592147b1fefbcba4df90b97a6bae4d7
Roc.Lai <laipeng668@qq.com>
2024-10-19 06:35:07 +0800

    Update test-script.sh

--- scripts/test-script.sh
+++ scripts/test-script.sh
@@ -1,3 +1,6 @@
+# 修改默认IP
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
 # 移除要替换的包
 rm -rf feeds/packages/net/alist
 rm -rf feeds/luci/applications/luci-app-alist
