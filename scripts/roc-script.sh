af4b1d8122f01616040e9119fba377c7d64db947
Roc.Lai <laipeng668@qq.com>
2024-10-20 13:10:54 +0800

    Update roc-script.sh

--- scripts/roc-script.sh
+++ scripts/roc-script.sh
@@ -4,4 +4,9 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 # 移除要替换的包
 rm -rf feeds/packages/net/alist
 rm -rf feeds/luci/applications/luci-app-alist
-rm -rf feeds/packages/net/adguardhome
+
+# Alist
+git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
+
+./scripts/feeds update -a
+./scripts/feeds install -a
