4a1c5bf2e969471dcb577f960db4b5a6a575acc9
Roc.Lai <laipeng668@qq.com>
2024-10-20 12:42:29 +0800

    Update roc-script.sh

--- scripts/roc-script.sh
+++ scripts/roc-script.sh
@@ -1,13 +1,3 @@
-# Alist
-# git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
-
-# 添加软件仓库
-sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-
-./scripts/feeds clean
-./scripts/feeds update -a
-./scripts/feeds install -a
-
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
