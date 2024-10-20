c40862a2dd864709f63e62f855eec857feca1bde
Roc.Lai <laipeng668@qq.com>
2024-10-20 12:38:02 +0800

    Update roc-script.sh

--- scripts/roc-script.sh
+++ scripts/roc-script.sh
@@ -1,11 +1,3 @@
-# 修改默认IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-
-# 移除要替换的包
-rm -rf feeds/packages/net/alist
-rm -rf feeds/luci/applications/luci-app-alist
-rm -rf feeds/packages/net/adguardhome
-
 # Alist
 # git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
 
@@ -15,3 +7,11 @@ sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.con
 ./scripts/feeds clean
 ./scripts/feeds update -a
 ./scripts/feeds install -a
+
+# 修改默认IP
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# 移除要替换的包
+rm -rf feeds/packages/net/alist
+rm -rf feeds/luci/applications/luci-app-alist
+rm -rf feeds/packages/net/adguardhome
