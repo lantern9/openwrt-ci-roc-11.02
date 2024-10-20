bbff6a223878ddedc9a9c20703d00de42d99ccc5
Roc.Lai <laipeng668@qq.com>
2024-10-20 08:46:25 +0800

    Update roc-script.sh

--- scripts/roc-script.sh
+++ scripts/roc-script.sh
@@ -9,8 +9,7 @@ rm -rf feeds/luci/applications/luci-app-alist
 # git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
 
 # 添加软件仓库
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 git pull
 
 ./scripts/feeds update -a
