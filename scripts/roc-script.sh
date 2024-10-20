bb7d0bc6833b9d1a8cb582af5cda006c71fc39ce
Roc.Lai <laipeng668@qq.com>
2024-10-20 13:48:09 +0800

    Update roc-script.sh

--- scripts/roc-script.sh
+++ scripts/roc-script.sh
@@ -6,7 +6,17 @@ rm -rf feeds/packages/net/alist
 rm -rf feeds/luci/applications/luci-app-alist
 rm -rf feeds/packages/net/adguardhome
 
-# Alist
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+
+# Alist & Adguardhome
 git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
 git_sparse_clone main https://github.com/kenzok8/small-package adguardhome luci-app-adguardhome
 
