cca11707b545da1780ae930de76642d04b50b762
Roc.Lai <laipeng668@qq.com>
2024-10-20 13:20:04 +0800

    Update roc-script.sh

--- scripts/roc-script.sh
+++ scripts/roc-script.sh
@@ -7,6 +7,7 @@ rm -rf feeds/luci/applications/luci-app-alist
 
 # Alist
 git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
+git clone https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
