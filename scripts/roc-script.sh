65c3308aa2b62da4ccdffe2edeebef3d2e2b0ed2
Roc.Lai <laipeng668@qq.com>
2024-10-22 10:01:14 +0800

    Update roc-script.sh

--- scripts/roc-script.sh
+++ scripts/roc-script.sh
@@ -4,7 +4,6 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 # 移除要替换的包
 rm -rf feeds/packages/net/alist
 rm -rf feeds/luci/applications/luci-app-alist
-rm -rf feeds/packages/net/adguardhome
 
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
@@ -18,7 +17,7 @@ function git_sparse_clone() {
 
 # Alist & Adguardhome & 集客无线AC控制器
 git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
-git_sparse_clone main https://github.com/kenzok8/small-package adguardhome luci-app-adguardhome
+git_sparse_clone main https://github.com/kenzok8/small-package luci-app-adguardhome
 git clone https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
 
 ./scripts/feeds update -a
