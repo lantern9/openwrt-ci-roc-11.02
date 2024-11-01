deca8cc4e6c400631f83b8021612bbc289d5f70b
Roc Lai <laipeng668@qq.com>
2024-11-01 08:19:39 +0800

    脚本注入亚瑟LED为绿色

--- scripts/roc-script.sh
+++ scripts/roc-script.sh
@@ -2,7 +2,6 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # 修改亚瑟LED为绿色
-<<<<<<< HEAD
 sed -i 's/led-running = &status_blue/led-running = &status_green/g' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6018-jdcloud-ax1800-pro.dts
 
 # 移除要替换的包
